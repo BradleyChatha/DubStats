64b0690b1a4641de75fc10e5e98a6a193e47f483
SealabJaster <sealabjaster@gmail.com>
2020-10-10 02:34:35 +0100

    Add new debug component

---
+++ frontend/src/components/GridDisplay.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="grid container">
+    <div class="grid column spanning-1" v-for="colClass in colourClasses" :key="colClass">
+      <div :class="colClass + ' box'"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "GridDebug",
+
+  data: function() {
+    return {
+      colourClasses: [
+        "primary-100",
+        "primary-200",
+        "primary-300",
+        "primary-400",
+        "primary-500",
+        "primary-600",
+        "primary-700",
+        "primary-800",
+        "primary-900",
+        "accent-100",
+        "accent-200",
+        "accent-300"
+      ]
+    };
+  }
+});
+</script>
+
+<style scoped lang="scss">
+@import "@/styles/global.scss";
+
+.box {
+  width: 100%;
+  height: 100%;
+}
+
+.container {
+  height: 100%;
+  min-height: 50px;
+}
+
+@each $colour, $values in $COLOURS {
+  @if type-of($values) == map {
+    @each $weight, $weight-value in $values {
+      .#{$colour}-#{$weight} {
+        background-color: $weight-value;
+      }
+    }
+  }
+}
+</style>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,15 +1,18 @@
 <template>
   <div>
-    <ColourDebug></ColourDebug>
+    <colour-debug></colour-debug>
+    <grid-debug></grid-debug>
   </div>
 </template>
 
 <script>
 import ColourDebug from "@/components/ColourDisplay.vue";
+import GridDebug from "@/components/GridDisplay.vue";
 
 export default {
   components: {
-    ColourDebug
+    ColourDebug,
+    GridDebug
   }
 };
 </script>
